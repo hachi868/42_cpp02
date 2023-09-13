1bf1ccd3fbf48124a2792991d0567576261d83fc
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-13 15:26:06 +0900

    :sparkles:[add]base

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,2 +1,32 @@
 
 #include "Fixed.hpp"
+Fixed::Fixed() : _numFixed(0), _bitsFra(8)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &obj) : _numFixed(obj._numFixed), _bitsFra(8)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::Fixed &operator = (const Fixed &obj)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "" << std::endl;//put
+}
+
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,6 +1,18 @@
-
-
 #ifndef __FIXED_H__
 #define __FIXED_H__
+
+class Fixed {
+public:
+	Fixed();
+	Fixed::Fixed(const Fixed &obj);
+	Fixed &operator = (const Fixed &obj);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+private:
+	int	_numFixed;
+	static const int	_bitsFra;
+
+};
 
 #endif //__FIXED_H__
