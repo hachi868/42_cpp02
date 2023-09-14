9f357232d4fc46575d7e2547b173705a61c4784b
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-14 11:19:03 +0900

    :sparkles:[add]ex02

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -1,8 +1,46 @@
-//
-// Created by hco on 2023/09/08.
-//
-
 #ifndef __FIXED_H__
 #define __FIXED_H__
+
+class Fixed {
+public:
+	Fixed();
+	Fixed(const Fixed &obj);
+	Fixed(const int num_i);
+	Fixed(const float num_fl);
+	Fixed &operator = (const Fixed &obj);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+
+	bool &operator > (const Fixed &obj) const;
+	bool &operator < (const Fixed &obj) const;
+	bool &operator >= (const Fixed &obj) const;
+	bool &operator <= (const Fixed &obj) const;
+	bool &operator == (const Fixed &obj) const;
+	bool &operator != (const Fixed &obj) const;
+
+	Fixed &operator + (const Fixed &obj);
+	Fixed &operator - (const Fixed &obj);
+	Fixed &operator * (const Fixed &obj);
+	Fixed &operator / (const Fixed &obj);
+
+	Fixed &operator ++ (const Fixed &obj);
+	Fixed &operator -- (const Fixed &obj);
+	Fixed &operator ++ (void);
+	Fixed &operator -- (void);
+
+	Fixed &min(Fixed &obj1, Fixed &obj2) const;
+	Fixed &min(const Fixed &obj1, const Fixed &obj2) const;
+	Fixed &max(Fixed &obj1, Fixed &obj2) const;
+	Fixed &max(const Fixed &obj1, const Fixed &obj2) const;
+
+private:
+	int _rawBit;
+	static const int	_bitsFra;
+};
+
+std::ostream &operator << (std::ostream &c_out, const Fixed &obj);
 
 #endif //__FIXED_H__
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Fixed.hpp"
+
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
